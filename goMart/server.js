48865c23985253c709639570280f7b0f77af1d59
Selvakumar <selvakumard274@gmail.com>
2024-05-08 16:53:33 +0530

    Initial Upload by selvakumar

---
+++ goMart/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();const PORT 
+=process.env.PORT|| 3001;
+
+const cors = require("cors");
+
+const productData=require('./MockData.json')
+
+app.use(cors());
+
+app.use(express.json());
+
+app.get("/test",(req,res)=>{
+    res.send(productData)
+})
+
+app.get("/test/:companyname",(req,res)=>{
+    const companyName=req.params.companyname;
+    const company=productData[0].companyName;
+    res.send(company);
+})
+
+if(process.env.NODE_ENV==='production'){
+  app.use(express.static('client/build'));
+}
+
+app.listen(PORT, () => {
+  console.log("Server running "+PORT);
+});
